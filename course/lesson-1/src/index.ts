8301a9f16eea1e26bf868e347db5b6175a8ce142
evaristebernhard <evaristebernhardwiener@gmail.com>
2026-01-07 20:53:47 +0800

    第一节课的图片

--- course/lesson-1/src/index.ts
+++ course/lesson-1/src/index.ts
@@ -2,11 +2,11 @@ import { ethers } from "ethers";
 
 async function main() {
     // faucet https://faucet.polkadot.io/ Paseo AssetHub
-    const URL = "https://services.polkadothub-rpc.com/testnet";
+    const URL = "https://testnet-passet-hub-eth-rpc.polkadot.io";
     const provider = new ethers.JsonRpcProvider(URL);
-    const address = "0x"; // put your address here
+    const address = "0xB9E24207CE8e801dfb168FC4Aa7C62a7a6796f6D"; // put your address here
     const balance = await provider.getBalance(address);
-    console.log(`Balance of ${address}: ${ethers.formatEther(balance)} ETH`);
+    console.log(`Balance of ${address}: ${ethers.formatEther(balance)} pas`);
 }
 
 main();
